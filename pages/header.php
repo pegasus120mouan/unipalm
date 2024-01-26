d6a0d832d5bd8c827903d4e6740087ad26e1b0e8
pegasus120mouan <coulibaly.fisher@yahoo.com>
2024-01-26 20:01:20 +0000

    updated260120241

--- pages/header.php
+++ pages/header.php
@@ -460,7 +460,7 @@ if (!isset($_SESSION['user_id'])) {
               <i class="fa fa-arrow-right"></i>
 
                 <p>
-                  Déconnexion
+                  Déconnexion1
                 </p>
               </a>
            </li>
