096befbc45bf6970ebde2347bc8510f90dd64928
pegasus120mouan <coulibaly.fisher@yahoo.com>
2024-03-01 14:42:35 +0000

    updated

--- pages/traitement_depot_clients.php
+++ pages/traitement_depot_clients.php
@@ -1,19 +1,20 @@
 <?php
-require('../fpdf/fpdf.php'); 
-require_once '../inc/functions/connexion.php';   
+require('../fpdf/fpdf.php');
+require_once '../inc/functions/connexion.php';
 
-if (isset($_POST['client']) && isset($_POST['date_debut']) && isset($_POST['date_fin']) ) {
+if (isset($_POST['client']) && isset($_POST['date_debut']) && isset($_POST['date_fin'])) {
     $client = $_POST['client'];
+    
     $date_debut = $_POST['date_debut'];
     $date_fin = $_POST['date_fin'];
 
-    // Étape 1 : Etablir la connexion à la base de données
+    $date_debut_fr = date('d/m/Y', strtotime($date_debut));
+    $date_fin_fr = date('d/m/Y', strtotime($date_fin));
 
-    // Étape 2 : Exécuter la requête SQL pour récupérer les données du pays et de la date sélectionnée
+    // Execute SQL query to fetch data
     $sql = "SELECT
-        b.id AS id_boutique,
         b.nom AS nom_boutique,
-        c.date_commande as date_commande,
+        c.date_commande AS date_commande,
         SUM(c.cout_reel) AS cout_reel_journalier
     FROM
         boutiques b
@@ -22,11 +23,11 @@ if (isset($_POST['client']) && isset($_POST['date_debut']) && isset($_POST['date
     JOIN
         commandes c ON u.id = c.utilisateur_id
     WHERE
-        c.date_commande BETWEEN :dateDebut AND :dateFin  AND b.nom= :client 
+        c.date_commande BETWEEN :dateDebut AND :dateFin  AND b.nom = :client 
     GROUP BY
-        b.id, b.nom, c.date_commande
+        b.nom, c.date_commande
     ORDER BY
-        b.id, c.date_commande";
+        b.nom, c.date_commande";
 
     $requete = $conn->prepare($sql);
     $requete->bindParam(':client', $client);
@@ -35,51 +36,60 @@ if (isset($_POST['client']) && isset($_POST['date_debut']) && isset($_POST['date
     $requete->execute();
     $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-    // Étape 3 : Créez un fichier PDF
+    // Create PDF
     $pdf = new FPDF();
     $pdf->AddPage();
 
-    // Définissez la police et la taille de la police
-    $pdf->SetFont('Arial', 'I', 14);
+    // Title
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(0, 10, utf8_decode('Rapport des dépots effectués'), 1, 1, 'C');
 
-    // Ajoutez un titre
-    $pdf->SetY(55);
-    $pdf->SetX(10);
-    $pdf->SetFont('Helvetica','B',12);
-    $pdf->Cell(50,10,utf8_decode("Point des dépots effectués"),0,1);
-    $pdf->SetFont('Helvetica','',12);
-    $pdf->Cell(50,7,"$client",0,1);
-    $pdf->SetFont('Helvetica','',12);
-    $pdf->Cell(50,7,"Du: $date_debut",0,1);
-    $pdf->Cell(50,7,"Au: $date_fin",0,1);
-    $pdf->Ln(7);
 
-    // Ajoutez les données dans le PDF
-    $pdf->SetFont('Helvetica', '', 12);
-    $pdf->SetFillColor(200, 200, 200);
-    $pdf->Cell(50, 10, "Partenaires", 1, 0, 'C', 1);
-    $pdf->Cell(50, 10, "Date", 1, 0, 'C', 1);
-    $pdf->Cell(50, 10, "Montant", 1, 1, 'C', 1);
+    // Client and date range
+    $pdf->SetFont('Arial', '', 12); // Set font to regular
+    $pdf->Cell(0, 10, "Partenaire: $client", 0, 1, 'L'); // Regular text
+    
+    $pdf->SetFont('Arial', 'B', 12); // Set font to bold
+    $pdf->Cell(20, 10, "Du: ", 0, 0, 'L'); // Bold text
+    $pdf->SetFont('Arial', '', 12); // Reset font to regular
+    $pdf->Cell(50, 10, "$date_debut_fr", 0, 0, 'L'); // Regular text
+    $pdf->SetFont('Arial', 'B', 12); // Set font to bold
+    $pdf->Cell(20, 10, "au: ", 0, 0, 'L'); // Bold text
+    $pdf->SetFont('Arial', '', 12); // Reset font to regular
+    $pdf->Cell(0, 10, "$date_fin_fr", 0, 1, 'L'); // Regular text
 
+    // Table headers
+    $pdf->SetFont('Arial', 'B', 12);
+    $pdf->SetFillColor(192); // Gray color for background
+    $pdf->Cell(60, 10, 'Boutique', 1, 0, 'C', true); // true indicates fill
+    $pdf->Cell(60, 10, 'Date', 1, 0, 'C', true); // true indicates fill
+    $pdf->Cell(60, 10, 'Montant', 1, 1, 'C', true); // true indicates fill
+    $pdf->SetFillColor(255); // Reset fill color
+
+    // Data
+    $pdf->SetFont('Arial', '', 12);
     $total = 0;
     foreach ($resultat as $row) {
-        $total = $total + $row['cout_reel_journalier'];
-        $pdf->Cell(50, 10, utf8_decode($row['nom_boutique']), 1);
-        $pdf->Cell(50, 10, $row['date_commande'], 1);
-        $pdf->Cell(50, 10, $row['cout_reel_journalier'], 1);
-        $pdf->Ln();
+        $total += $row['cout_reel_journalier'];
+        $pdf->Cell(60, 10, $row['nom_boutique'], 1, 0, 'C');
+        $date_commande_fr = date('d-m-Y', strtotime($row['date_commande']));
+        $pdf->Cell(60, 10, $date_commande_fr, 1, 0, 'C');
+        $pdf->Cell(60, 10, $row['cout_reel_journalier'], 1, 1, 'C');
     }
+
+    // Total
     
-    $pdf->SetFillColor(173, 216, 230);
-    $pdf->Cell(50, 10, "Total", 1, 0, 'C', 1);
-    $pdf->SetFillColor(173, 216, 230);
-    $pdf->SetFont('Helvetica', '', 25);
-    $pdf->Cell(100, 10, $total, 1,0,'C',1);
 
-    // Étape 4 : Générez le fichier PDF
+    $pdf->SetFont('Arial', 'B', 20);
+    $pdf->SetFillColor(0); // Black background
+    $pdf->SetTextColor(255); // White text
+    $pdf->Cell(120, 10, 'Total', 1, 0, 'R', true); // true for filling cell
+    $pdf->Cell(60, 10, $total, 1, 1, 'C', true); // true for filling cell
+
+    // Output PDF
     $pdf->Output();
-
-    // Étape 5 : Fermer la connexion à la base de données
+    
+    // Close database connection
     $conn = null;
 } else {
     echo "Veuillez sélectionner un client et une date.";
