acf0049dd2346a6196e480c953c91f69e6e2278a
pegasus120mouan <coulibaly.fisher@yahoo.com>
2024-06-19 20:41:37 +0000

    updated180620242

--- pages/analytics/vuegenerale_colis_nonlivres_clients.php
+++ pages/analytics/vuegenerale_colis_nonlivres_clients.php
@@ -202,7 +202,8 @@ $commandes_livres_clients = $stmt->fetchAll();
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="../dossier_avatars/<?php echo $_SESSION['avatar']; ?>" class="img-circle elevation-2" alt="Logo">
+            <img src="../../dossiers_images/<?php echo $_SESSION['avatar']; ?>" class="img-circle elevation-2" alt="Logo">
+            
             <!-- <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">-->
           </div>
           <div class="info">
